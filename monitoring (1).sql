afa867bb0967336349a3a9a4c6ca3c312c8cc1ac
Alro John <alromercado08@gmail.com>
2025-03-28 09:45:01 +0800

    asdf

--- monitoring (1).sql
+++ monitoring (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2025 at 09:45 AM
+-- Generation Time: Mar 28, 2025 at 02:44 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -49,7 +49,12 @@ INSERT INTO `location` (`LOCATION_ID`, `STREET`, `PUROK`, `BARANGAY`) VALUES
 (195, NULL, '5', 'Salvacion'),
 (196, NULL, '4', 'Salvacion'),
 (197, NULL, '4', 'Salvacion'),
-(198, NULL, '4', 'test');
+(198, NULL, '4', 'test'),
+(199, NULL, '4', 'Salvacion'),
+(200, NULL, '4', 'Salvacion'),
+(201, NULL, '4', 'Salvacion'),
+(202, NULL, '4', 'Salvacion'),
+(203, NULL, '4', 'Salvacion');
 
 -- --------------------------------------------------------
 
@@ -62,6 +67,8 @@ CREATE TABLE `owners` (
   `SHOP_ID` int(11) NOT NULL,
   `FIRST_NAME` varchar(50) DEFAULT NULL,
   `LAST_NAME` varchar(50) DEFAULT NULL,
+  `MIDDLE_NAME` varchar(50) NOT NULL,
+  `SUFFIX` varchar(50) NOT NULL,
   `GENDER` varchar(50) DEFAULT NULL,
   `EMAIL` varchar(100) DEFAULT NULL,
   `PHONE_NUMBER` varchar(20) NOT NULL,
@@ -77,14 +84,15 @@ CREATE TABLE `owners` (
 -- Dumping data for table `owners`
 --
 
-INSERT INTO `owners` (`OWNER_ID`, `SHOP_ID`, `FIRST_NAME`, `LAST_NAME`, `GENDER`, `EMAIL`, `PHONE_NUMBER`, `JOB_ID`, `HIRED_DATE`, `LOCATION_ID`, `DOCUMENT_1`, `DOCUMENT_2`, `STATUS`) VALUES
-(1, 1, 'Admin', 'Admin', 'Male', 'Admin@email.com', '09991234567', 0, '2025-02-05', 188, '../assets/documents/717348001.png', '../assets/documents/717348002.png', 'Active'),
-(18488, 20670572, 'Samarina', 'Hardware', 'Male', 'samarina2@gmail.com', '09815128418', 0, '2025-03-11', 190, '../assets/documents/206705721.jpg', '../assets/documents/206705722.jpg', 'Active'),
-(21338, 42302750, 'Buddy', 'Benasa', 'Male', 'buddybenasa12@gmail.com', '09164166702', 0, '2025-03-11', 193, '../assets/documents/423027501.jpg', '../assets/documents/423027502.jpg', 'Active'),
-(34171, 56772380, 'Jojo', 'marjalino', 'Male', 'jojomarjalino@wgmail.com', '09164166702', 0, '2025-03-15', 194, '../assets/documents/567723801.jpg', '../assets/documents/567723802.jpg', 'Active'),
-(47588, 77981742, 'Richard', 'Alipio', 'Male', 'alipio123@gmail.com', '09164166702', 0, '2025-03-11', 191, '../assets/documents/779817421.jpg', '../assets/documents/779817422.jpg', 'Active'),
-(84785, 67692435, 'jmic', 'malto', 'Male', 'jmic@gmail.com', '09815128418', 0, '2025-03-15', 195, '../assets/documents/676924351.png', '../assets/documents/676924352.png', 'Active'),
-(90452, 73798598, 'Ribancos', 'store', 'Male', 'ribancos2@gmail.com', '09815128418', 0, '2025-03-11', 192, '../assets/documents/737985981.png', '../assets/documents/737985982.jpg', 'Active');
+INSERT INTO `owners` (`OWNER_ID`, `SHOP_ID`, `FIRST_NAME`, `LAST_NAME`, `MIDDLE_NAME`, `SUFFIX`, `GENDER`, `EMAIL`, `PHONE_NUMBER`, `JOB_ID`, `HIRED_DATE`, `LOCATION_ID`, `DOCUMENT_1`, `DOCUMENT_2`, `STATUS`) VALUES
+(1, 1, 'Admin', 'Admin', '', '', 'Male', 'Admin@email.com', '09991234567', 0, '2025-02-05', 188, '../assets/documents/717348001.png', '../assets/documents/717348002.png', 'Active'),
+(18488, 20670572, 'Samarina', 'Hardware', '', '', 'Male', 'samarina2@gmail.com', '09815128418', 0, '2025-03-11', 190, '../assets/documents/206705721.jpg', '../assets/documents/206705722.jpg', 'Active'),
+(21338, 42302750, 'Buddy', 'Benasa', '', '', 'Male', 'buddybenasa12@gmail.com', '09164166702', 0, '2025-03-11', 193, '../assets/documents/423027501.jpg', '../assets/documents/423027502.jpg', 'Active'),
+(34171, 56772380, 'Jojo', 'marjalino', '', '', 'Male', 'jojomarjalino@wgmail.com', '09164166702', 0, '2025-03-15', 194, '../assets/documents/567723801.jpg', '../assets/documents/567723802.jpg', 'Active'),
+(47588, 77981742, 'Richard', 'Alipio', '', '', 'Male', 'alipio123@gmail.com', '09164166702', 0, '2025-03-11', 191, '../assets/documents/779817421.jpg', '../assets/documents/779817422.jpg', 'Active'),
+(81930, 61374502, 'test', 'test', 'test', 'test', 'Male', 'test@gmail.com', '9665376905', 0, '2025-03-28', 203, '../assets/documents/613745021.png', '../assets/documents/613745022.png', 'Pending'),
+(84785, 67692435, 'jmic', 'malto', '', '', 'Male', 'jmic@gmail.com', '09815128418', 0, '2025-03-15', 195, '../assets/documents/676924351.png', '../assets/documents/676924352.png', 'Active'),
+(90452, 73798598, 'Ribancos', 'store', '', '', 'Male', 'ribancos2@gmail.com', '09815128418', 0, '2025-03-11', 192, '../assets/documents/737985981.png', '../assets/documents/737985982.jpg', 'Active');
 
 -- --------------------------------------------------------
 
@@ -204,7 +212,8 @@ INSERT INTO `stores` (`SHOP_ID`, `STORE_ID`, `SHOP_NAME`, `ADDRESS`, `IMAGE`, `A
 (20670572, 3, 'SAMARINA CONTRUCTION HARDWARE', 'Legazpi City Albay', 0x616c6970696f2e6a7067, 'Open', '2025-03-11 07:55:05', '05:00', '19:00', 3),
 (42302750, 1, 'A. BOSEO', '', 0x626f73656f2e6a7067, 'Open', '2025-03-11 21:27:06', '05:00', '17:00', 5),
 (67692435, 1, 'JMIC MEAT SHOP', '', 0x53637265656e73686f7420323032352d30332d3133203131343834342e706e67, 'Open', '2025-03-15 02:48:00', '05:00 AM', '12:00 PM', 7),
-(77981742, 2, 'ALIPIO STORE', 'Legazpi City ', 0x616c6970696f2e6a7067, '', '2025-03-10 23:55:05', '05:00', '19:00', 8);
+(77981742, 2, 'ALIPIO STORE', 'Legazpi City ', 0x616c6970696f2e6a7067, '', '2025-03-10 23:55:05', '05:00', '19:00', 8),
+(61374502, 3, 'Test', NULL, NULL, 'Open', '2025-03-28 01:39:50', '05:00 AM', '12:00 PM', 12);
 
 -- --------------------------------------------------------
 
@@ -329,7 +338,8 @@ INSERT INTO `users` (`ID`, `SHOP_ID`, `OWNER_ID`, `USERNAME`, `PASSWORD`, `TYPE_
 (39, 20670572, 18488, 'samarina', 'samarina123', 3, 3),
 (40, 77981742, 47588, 'alipio', 'alipio123', 2, 2),
 (42, 42302750, 21338, 'buddy', 'buddy123', 1, 1),
-(44, 67692435, 84785, 'malto', 'malto123', 1, 1);
+(44, 67692435, 84785, 'malto', 'malto123', 1, 1),
+(49, 61374502, 81930, 'test', 'test', 3, 3);
 
 --
 -- Indexes for dumped tables
@@ -411,7 +421,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `LOCATION_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
+  MODIFY `LOCATION_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -429,7 +439,7 @@ ALTER TABLE `reservations`
 -- AUTO_INCREMENT for table `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `store_locations`
@@ -453,7 +463,7 @@ ALTER TABLE `supplier`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
